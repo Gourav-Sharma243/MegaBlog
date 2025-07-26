fecb7ba51fcb2f538b83ed7d744f15a9a02807a9
Gourav-Sharma243 <147566196+Gourav-Sharma243@users.noreply.github.com>
2025-07-25 17:03:50 -0700

    logo update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-function Logo({width = '100px'}) {
+function Logo({ width = '120px', alt = 'MegaBlog Logo' }) {
   return (
-    <div>Logo</div>
-  )
+    <div style={{ width }}>
+      <img
+        src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png"
+        alt={alt}
+        style={{ width: '100%', height: 'auto' }}
+      />
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
