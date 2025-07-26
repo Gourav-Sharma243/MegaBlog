1817b1a91453c5bd8af93f929f7d34d21bbee7a2
Gourav-Sharma243 <147566196+Gourav-Sharma243@users.noreply.github.com>
2025-07-25 17:06:04 -0700

    updtae

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,6 +7,7 @@ function Logo({ width = '120px', alt = 'MegaBlog Logo' }) {
         src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png"
         alt={alt}
         style={{ width: '100%', height: 'auto' }}
+        className='rounded-2xl'
       />
     </div>
   );
